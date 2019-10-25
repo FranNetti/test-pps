0f1c1110e45363a3548a462baf064f038e28c155
andrix17 <50406991+andrix17@users.noreply.github.com>
2019-10-25 18:15:31 +0200

    feature-Person enhancement

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,22 +1,32 @@
+object Gender extends Enumeration {
+  val Man, Woman = Value
+}
 
 sealed trait Person {
   def name: String
   def surname: String
   def address: String
+  def phoneNumber: Int
+  def gender: Gender
+  def phoneNumber_=(phoneNumber: Int): Unit
   def address_=(addr: String): Unit
 }
 
-case class Student(name:String, surname: String, private var _address: String) extends Person {
+case class Student(name:String, surname: String, gender: Gender, private var _phoneNumber: Int, private var _address: String) extends Person {
 
   override def address: String = this._address
 
   override def address_=(addr: String): Unit = this._address = addr
+  
+  override def phoneNumber: Int = this._phoneNumber
+
+  override def phoneNumber_=(phone: Int): Unit = this._phoneNumber = phone
 }
 
 
 object TestApp extends App {
 
-  val student = Student("Francesco", "Grandinetti", "via x")
+  val student = Student("Francesco", "Grandinetti", Gender.Man, 666, "via x")
 
   student.address = "Via zaccherini 25"
 
